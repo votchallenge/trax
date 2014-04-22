b297faacc5d123f2a947aa060f8292e913c76a2e
Luka Čehovin <luka.cehovin@gmail.com>
2014-04-22 15:56:59 +0200

    Matlab compilation more Windows friendly.

--- matlab/compile_trax.m
+++ matlab/compile_trax.m
@@ -1,10 +1,25 @@
 
 trax_path = fileparts(fileparts(mfilename('fullpath')));
 
-arguments = {['-I', fullfile(trax_path, 'lib')], fullfile(trax_path, 'matlab', 'mwrapper.cpp'), '-ltraxstatic', ['-L', fullfile(trax_path, 'build')]};
-name = 'mwrapper';
-mex('-f', fullfile(matlabroot, 'bin', 'engopts.sh'), '-output', name, arguments{:});
+% TODO: how to automate this part? Please specify the configuration
+% manually if using a different compiler.
+if ispc
+    lib_path = fullfile(trax_path, 'build', 'Debug');
+    engopts = fullfile(matlabroot, 'bin', 'win64', 'mexopts', 'mssdk71engmatopts.bat');
+else
+    lib_path = fullfile(trax_path, 'build');
+    engopts = fullfile(matlabroot, 'bin', 'engopts.sh');
+end;
 
-arguments = {['-I', fullfile(trax_path, 'lib')], fullfile(trax_path, 'matlab', 'traxserver.cpp'), '-ltraxstatic', ['-L', fullfile(trax_path, 'build')]};
+disp('Building mwrapper ...');
+arguments = {['-I', fullfile(trax_path, 'lib')], fullfile(trax_path, 'matlab', 'mwrapper.cpp'), '-ltraxstatic', ['-L', lib_path]};
+mex('-f', engopts, '-output', 'mwrapper', arguments{:});
 
-mex(arguments{:});
+% Only build traxserver on Unix systems
+if ~ispc
+
+    disp('Building traxserver ...');
+    arguments = {['-I', fullfile(trax_path, 'lib')], fullfile(trax_path, 'matlab', 'traxserver.cpp'), '-ltraxstatic', ['-L', lib_path]};
+    mex(arguments{:});
+    
+end;
