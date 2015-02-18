738b74f3cb5625ced4a8790f0529c26be49c1435
Luka Čehovin <luka.cehovin@gmail.com>
2015-02-18 11:55:31 +0100

    Fixes #3

--- lib/region.h
+++ lib/region.h
@@ -10,11 +10,11 @@
 #endif
 
 #ifndef MAX
-#define MAX(a,b) ((a) > (b)) ? (a) : (b)
+#define MAX(a,b) (((a) > (b)) ? (a) : (b))
 #endif
 
 #ifndef MIN
-#define MIN(a,b) ((a) < (b)) ? (a) : (b)
+#define MIN(a,b) (((a) < (b)) ? (a) : (b))
 #endif
 
 #ifdef __cplusplus
