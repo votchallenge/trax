80f0a0a98f4aa174f95cb2e1b0c45d51ee80d73f
Luka Čehovin <luka.cehovin@gmail.com>
2016-05-10 15:46:01 +0200

    Adding setup script for Python library.

---
+++ python/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='TraX',
+      version='1.0',
+      description='TraX protocol implementation for Python',
+      author='Luka Cehovin',
+      author_email='luka.cehovin@fri.uni-lj.si',
+      url='https://github.com/votchallenge/trax/',
+      packages=['trax'],
+     )
