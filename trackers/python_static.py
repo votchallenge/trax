90db3c2123bfd8da46d5e2c06c4d58e4141f16c5
Luka Čehovin <luka.cehovin@gmail.com>
2016-08-10 18:13:32 +0100

    Fixing python example

--- trackers/python_static.py
+++ trackers/python_static.py
@@ -1,11 +1,5 @@
 """
-\file python_static.py
-
-@brief Python implementation of the a static tracker
-
-@author Alessio Dore, Luka Cehovin
-
-@date 2016
+Example Python implementation of the a static tracker
 """
 
 import trax.server
@@ -13,18 +7,17 @@ import trax.region
 import trax.image
 import time
 
-if __name__ == '__main__':
-    options = trax.server.ServerOptions(trax.region.RECTANGLE, trax.image.PATH)
-    region = None
-    delay = 0
-    with trax.server.Server(options, verbose=True) as server:
-        while True:
-            request = server.wait()
-            if request.type in ["quit", "error"]:
-                break
-            if request.type == "initialize":
-                region = request.region
-                delay = float(request.parameters.get("time.wait", "0"))
-            server.status(region)
-            if delay > 0:
-                time.sleep(delay)
+options = trax.server.ServerOptions(trax.region.RECTANGLE, trax.image.MEMORY)
+region = None
+delay = 0
+with trax.server.Server(options, verbose=True) as server:
+    while True:
+        request = server.wait()
+        if request.type in ["quit", "error"]:
+            break
+        if request.type == "initialize":
+            region = request.region
+            delay = float(request.parameters.get("time.wait", "0"))
+        server.status(region)
+        if delay > 0:
+            time.sleep(delay)
