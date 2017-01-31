8738b64fc199b92965fcd74d4ee2a106482dc5c0
Luka Čehovin <luka.cehovin@gmail.com>
2017-01-31 22:19:44 +0100

    Matlab compile script also supported on Octave.

--- support/matlab/compile_trax.m
+++ support/matlab/compile_trax.m
@@ -1,6 +1,8 @@
-function [] = compile_trax()
+function [] = compile_trax(trax_path)
 
-trax_path = fileparts(fileparts(mfilename('fullpath')));
+if nargin < 1
+    trax_path = fileparts(fileparts(fileparts(mfilename('fullpath'))));
+end;
 
 if ispc
     lib_path = find_file_recursive(trax_path, 'traxstatic.lib');
@@ -8,13 +10,30 @@ else
     lib_path = find_file_recursive(trax_path, 'libtraxstatic.a');
 end;
 
+try
+    OCTAVE_VERSION;
+    isoctave = true;
+catch
+    isoctave = false;
+end
+
 if isempty(lib_path)
     error('TraX library not found. Stopping.');
 end;
 
 disp('Building MEX file ...');
-arguments = {['-I', fullfile(trax_path, 'lib')], fullfile(trax_path, 'matlab', 'traxserver.cpp'), '-ltraxstatic', ['-L', lib_path]};
-mex(arguments{:});
+arguments = {['-I', fullfile(trax_path, 'include')], fullfile(trax_path, 'support', 'matlab', 'traxserver.cpp'), '-ltraxstatic', ['-L', lib_path]};
+
+if isoctave
+    [out, status] = mkoctfile('-mex', arguments{:});
+    if status
+        print_text('ERROR: Unable to compile MEX function.');
+        success = false;
+        return;
+    end;
+else
+    mex(arguments{:});
+end
 
 end
 
@@ -22,24 +41,24 @@ function [path] = find_file_recursive(root, filename)
     path = [];
 
     data = dir(root);
-    isdir = [data.isdir]; 
+    isdir = [data.isdir];
     files = {data(~isdir).name}';
     if ~isempty(files)
         for f = 1:numel(files)
-            
+
             if strcmp(files{f}, filename)
                 path = root;
                 return;
             end;
         end;
     end
-    
+
     dirs = {data(isdir).name};
     for f = 1:numel(dirs)
         if dirs{f}(1) == '.'
             continue;
         end;
-        path = find_file_recursive(fullfile(root, dirs{f}), filename); 
+        path = find_file_recursive(fullfile(root, dirs{f}), filename);
         if ~isempty(path)
             return;
         end;
