45ffec3c114fc11325e59dd7c1e1498f05bc944e
Luka Čehovin <luka.cehovin@gmail.com>
2017-04-14 16:13:14 +0200

    Improving client library timing utilities.

--- support/client/timer.cpp
+++ support/client/timer.cpp
@@ -1,9 +1,18 @@
 
 #include <stdio.h>
-#ifdef WIN32
+#if defined(__OS2__) || defined(__WINDOWS__) || defined(WIN32) || defined(WIN64) || defined(_MSC_VER)
+#define WINDOWS
 #include <windows.h>
 #else
+#ifdef _MAC_
+#define OSX
+#include <mach/mach.h>
+#include <mach/mach_time.h>
+static const uint64_t NANOS_PER_USEC = 1000ULL;
+#else
+#define LINUX
 #include <sys/time.h>
+#endif
 #endif
 #ifdef HAVE_STDINT_H
 #include <stdint.h>
@@ -18,35 +27,36 @@
 namespace trax {
 
 timer_state timer_clock() {
-#ifdef WIN32
+#if defined(WINDOWS)
     timer_state status;
     QueryPerformanceCounter ((LARGE_INTEGER *) &status);
     return status;
+#elif defined(OSX)
+    mach_timebase_info_data_t timebase_info;
+    mach_timebase_info(&timebase_info);
+    return (timer_state)((timebase_info.denom * NANOS_PER_USEC) / timebase_info.numer);
 #else
     struct timeval tv;
-
     gettimeofday (&tv, NULL);
-
     return ((timer_state) tv.tv_sec * TIMER_USEC_PER_SEC) + tv.tv_usec;
 #endif
 }
 
 double timer_elapsed(timer_state start) {
-#ifdef WIN32
+#if defined(WINDOWS)
     static timer_state freq = 0;
     timer_state delta, stop;
 
     if (freq == 0)
         QueryPerformanceFrequency ((LARGE_INTEGER *) &freq);
+
 #else
 #define freq TIMER_USEC_PER_SEC
     timer_state delta, stop;
 #endif
 
     stop = timer_clock();
-
     delta = stop - start;
-
     return (double) delta / (double) freq;
 }
 
