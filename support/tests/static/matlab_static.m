269119bc363da0b22ccf0a5429bf6c23614d9086
Luka Čehovin <luka.cehovin@gmail.com>
2017-05-12 00:39:07 +0200

    Updating Octave tests.

--- support/tests/static/matlab_static.m
+++ support/tests/static/matlab_static.m
@@ -2,23 +2,32 @@ function [] = matlab_static()
 
 cleanup = onCleanup(@() exit() );
 
-traxserver('setup', 'polygon', {'path', 'memory', 'data'});
+traxserver('setup', 'polygon', {'path', 'memory', 'buffer'});
 
 memory = [0 0 0 0];
 
+timeout = 0;
+
 while 1
 
-    [image, region] = traxserver('wait');
+    [image, region, properties] = traxserver('wait', 'PropStruct', true);
 
     if isempty(image)
 		break;
 	end;
 
 	if ~isempty(region)
+		if isfield(properties, 'time_wait')
+			timeout = str2double(properties.time_wait);
+		end;
         memory = region;
-    end
+    end;
+
+    if timeout > 0
+    	pause(timeout);
+    end;
 
 	traxserver('status', memory);
-end
+end;
 
 traxserver('quit');
